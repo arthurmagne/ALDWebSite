51e725af16bd199e28e9c59de4b7652513d5be96
arthurmagne <arthur.magne@hotmail.fr>
2013-12-28 18:15:47 +0100

    ajout de backbone.js et de template de base pour test, préparation de require.js

--- js/router.js
+++ js/router.js
@@ -1,15 +1,39 @@
-
-crossroads.addRoute('/{section}', function(section){
-});
-
-var a = document.querySelectorAll('a')
-for(i=0;i<a.length;i++){
-    a[i].onclick=function(e){
-        e.preventDefault();
-        console.log(this.href.split('/').pop());
-        crossroads.parse('/' + this.href.split('/').pop());
-        window.location.href = this.href;
-        window.history.pushState("object", "title", this.href.split('/').pop().split('.html'));
+// Filename: router.js
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/projects/list',
+  'views/users/list'
+], function($, _, Backbone, ProjectListView, UserListView){
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+        '': 'home',
+        'new': 'addOeuvre'
     }
-}
-crossroads.parse('/home');
+  });
+
+  var initialize = function(){
+    var app_router = new AppRouter;
+    app_router.on('showProjects', function(){
+      // Call render on the module we loaded in via the dependency array
+      // 'views/projects/list'
+      var projectListView = new ProjectListView();
+      projectListView.render();
+    });
+      // As above, call render on our loaded module
+      // 'views/users/list'
+    app_router.on('showUsers', function(){
+      var userListView = new UserListView();
+      userListView.render();
+    });
+    app_router.on('defaultAction', function(actions){
+      // We have no matching route, lets just log what the URL was
+      console.log('No route:', actions);
+    });
+    Backbone.history.start();
+  };
+  return {
+    initialize: initialize
+  };
+});
